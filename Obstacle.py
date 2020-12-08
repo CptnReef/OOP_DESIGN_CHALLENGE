b6733ad6a57b2c83d883bfadbfd3e04154156e89
Cptn Reef <sharif.staffordbz@gmail.com>
2020-12-07 21:31:00 -0500

    season finale commit

--- Obstacle.py
+++ Obstacle.py
@@ -1,16 +1,62 @@
 class Obstacle:
-    def __init__(self, move, interact):
-        self.move = move
-        self.interact = interact
-    
-    def board(self):
-        x = 15
-        y = 15
-        self.grid = (x,y)
+    def __init__(self, carry):
+        self.carry = dict(carry)
+        self.x = 1
+        self.y = 1
+                
+    def player_Move(self):
 
-    def object(self):
-        pass
+        self.start = input("Ready? [Yes/No]")
+        self.start = self.start.lower()
+        print(f"{self.start}\n")
         
-    def colliding(self):
-        pass
-        
+        if self.start == "yes":
+            while self.x and self.y != 15:
+
+
+                self.move = input()
+                self.move = self.move.lower()
+                
+                if self.move == str("w"):
+                    self.y += 1
+                    print(f"front step({self.x},{self.y})")
+                    self.move
+                elif self.move == str("s"):
+                    self.y -= 1
+                    print(f"back step ({self.x},{self.y})")
+                    self.move
+                elif self.move == str("d"):
+                    self.x += 1
+                    print(f"right step ({self.x},{self.y})")
+                    self.move
+                elif self.move == str("a"):
+                    self.x -= 1
+                    print(f"left step ({self.x},{self.y})")
+                    self.move
+                elif self.move == str("i"):
+                    print(f"{self.carry}")
+                    self.move
+                elif self.move == str("q"):
+                    if self.x == int(3) and self.y == int(14):
+                        print('you found a pair of "Bunny Slippers!"')
+                        self.carry["Bunny Slippers"] = 10
+                        print('Hm, comfy."')
+                    elif self.x == int(7) and self.y == int(14):
+                        print('Nerf or Nuffin! "Nerf Gun!"')
+                        self.carry["Nerf Gun"] = 100
+                        print('My arm is complete.')
+                    elif self.x == int(12) and self.y == int(1):
+                        print('Wow, you picked up a hairy "Mc Muffin!"')
+                        self.carry["McMuffin"] = 1000
+                        print('Mmm- crunchy.')
+                else:
+                    print("")
+                    
+                if self.y < int(1) or self.y >= int(15):
+                    print(f"oof!({self.x},{self.y})")
+                    break
+                elif self.x < int(1) or self.x >= int(15):
+                    print(f"oof!({self.x},{self.y})")
+                    break
+        else:
+            print("Bye.")
--- Player.py
+++ Player.py
@@ -3,30 +3,31 @@ from Obstacle import Obstacle
 
 class Player(Item, Obstacle):
 
-    def __init__(self, inventory_Id, inventory_value, inventory_Slot, available_Item, move, interact):
+    def __init__(self, inventory_Id, inventory_value, inventory_Slot, available_Item, x,y,name,start,move,carry):
         Item.__init__(self, inventory_Id, inventory_value, inventory_Slot, available_Item)
-        self.move = move
-        self.interact = interact
-        name = input("Enter Name:")
-        self.name = print(f"Hi {name}, We're going on an adventure!")
-        start = input("unless that's alright with you? [Yes/No]")
-        self.start = print(f"{start}...")
-
+        Obstacle.__init__(self, carry)
+        self.check_Inventory(self.carry)
+        self.name = input("Enter Name:\n")
+        self.name = self.name.lower()
+        
     def inventory_Item(self, pack, item):
         Item.inventory_Item(self, pack, item)
 
     def fixed_Position(self):
         Item.fixed_Position(self)
 
-    def pick_Up(self):
-        pass
-
+    def player_Move(self):
+        self.name
+        self.inventory_Item("",1)
+        print(" [W]up [S]down [A]left [D]right [I]check_items [Q]search_items")
+        Obstacle.player_Move(self)
+        
 if __name__ == "__main__":
-    bob = Player(333, 30, {"another_Slot":100}, {"available_Slot":100},"","")
+    bob = Player(333, 30, {"another_Slot":100}, {"available_Slot":100}, 1,1,"","","",{})
     # print(bob._value)
     # print(bob.inventory_Slot)
     # print(bob.available_Item)
     # bob.inventory_Item("",1)
     # bob.fixed_Position()
-    bob.name
-    bob.start
+    # bob.player_Move()
+    bob.player_Move()
